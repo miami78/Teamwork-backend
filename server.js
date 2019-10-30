26455582cc70ddbddc7908d81d422606b4cd56b2
miami78 <miamilarry78@gmail.com>
2019-10-30 16:17:04 +0300

    eslint rule changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 const http = require("http");
 const app = require("./app");
 
@@ -19,16 +20,17 @@ const errorHandler = error => {
   if (error.syscall !== "listen") {
     throw error;
   }
+  // eslint-disable-next-line no-use-before-define
   const address = server.address();
   const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
+    typeof address === "string" ? `pipe ${address}` : `port: ${port}`;
   switch (error.code) {
     case "EACCES":
-      console.error(bind + " requires elevated privileges.");
+      console.error(`${bind} requires elevated privileges.`);
       process.exit(1);
       break;
     case "EADDRINUSE":
-      console.error(bind + " is already in use.");
+      console.error(`${bind} is already in use.`);
       process.exit(1);
       break;
     default:
@@ -41,8 +43,8 @@ const server = http.createServer(app);
 server.on("error", errorHandler);
 server.on("listening", () => {
   const address = server.address();
-  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-  console.log("Listening on " + bind);
+  const bind = typeof address === "string" ? `pipe ${address}` : `port ${port}`;
+  console.log(`Listening on ${bind}`);
 });
 
 server.listen(port);
