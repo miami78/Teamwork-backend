618371b2a7edfd0e132d7b64395b403ba0cbc58b
miami78 <miamilarry78@gmail.com>
2019-10-31 07:27:52 +0300

    modified dependancies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const knex = require("knex");
+
 
 const db = knex({
   client: "pg",
@@ -17,5 +17,6 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
 
 module.exports = app;
