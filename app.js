ff91b97680210ae53f98ae3da8a6792b397af1fb
miami78 <miamilarry78@gmail.com>
2019-10-31 10:47:01 +0300

    function-admin can create an employee user account

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+// gets json response
+app.get("/", (req, res) => {
+  res.json({ message: "Server starts successfully!" });
+});
+// admin can create an employee user account
+app.post("/auth/create-user", (req, res, next) => {
+  res.status(201).json({
+    status: "success",
+    data: {
+      message: "User account successfully created!",
+      token: "",
+      userId: 4
+    }
+  });
+});
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("Teamwork", () => {
+  // gets json response
   describe("GET /", function() {
     it("responds with json", function() {
       return request(app)
