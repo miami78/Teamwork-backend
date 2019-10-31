6f811943fa109b05c8100878d1856e0825f1e777
miami78 <miamilarry78@gmail.com>
2019-10-31 10:26:28 +0300

    admin can create an employee user account test

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-
-const db = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "50filthyCENT!",
-    database: "teamwork-dev"
-  }
-});
 
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
 
 module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,58 @@
+const { expect } = require("chai");
+const request = require("supertest");
+const app = require("../app");
+
+describe("Teamwork", () => {
+  describe("GET /", function() {
+    it("responds with json", function() {
+      return request(app)
+        .get("/")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+  });
+
+  // Admin can create an employee user account
+  describe("POST /auth/create-user", function() {
+    it("returns status code 201", function(done) {
+      request(app)
+        .post("/auth/create-user")
+        .end(function(err, res) {
+          expect(res.status).to.equal(201);
+          done();
+        });
+    });
+    it("responds with json data containing status success", function(done) {
+      request(app)
+        .post("/auth/create-user")
+        .send({
+          firstName: "string",
+          lastName: "string",
+          email: "string",
+          password: "string",
+          gender: "string",
+          jobRole: "string",
+          department: "string",
+          address: "string"
+        })
+        .expect("Content-Type", /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          const {
+            body: {
+              status,
+              data: { message, token, userId }
+            }
+          } = res;
+          expect(res.status).to.equal(201);
+          expect(status).to.equal("success");
+          expect(message).to.equal("User account successfully created!");
+          expect(token).to.be.a("string");
+          expect(userId).to.be.a("number");
+          expect(userId % 1).to.equal(0);
+          done();
+        });
+    });
+  });
+});
