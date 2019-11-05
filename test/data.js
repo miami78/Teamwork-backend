3ce1170dd2ad131e091ea4f0ae13d122d3219ee4
miami78 <miamilarry78@gmail.com>
2019-11-05 15:15:28 +0300

    major changes

---
+++ test/data.js
@@ -0,0 +1,11 @@
+// Employee account creation a
+exports.testUser = {
+  firstName: "Jane",
+  lastName: "Doe",
+  email: "example@example.com",
+  password: "password",
+  gender: "Female",
+  jobRole: "Software Developer",
+  department: "Information Technology",
+  address: "Nairobi, Kenya"
+};
